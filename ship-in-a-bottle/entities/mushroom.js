6103f5c6940a8ebf2d3937637688e744c9fbfbe6
Bernard Mordan <bernardmordan@gmail.com>
2022-07-23 15:15:54 +0100

    welcome to the construct

--- ship-in-a-bottle/entities/mushroom.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('mushroom', {
-    init: function () {
-        this.geometry = new THREE.BoxBufferGeometry(1,1,1)
-        this.material = new THREE.MeshStandardMaterial({color: '#fcba03'})
-        this.mesh = new THREE.Mesh(this.geometry, this.material)
-        this.el.setObject3D('mesh', this.mesh)
-    }
-})
